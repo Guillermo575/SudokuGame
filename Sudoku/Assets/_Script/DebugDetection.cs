85e27c3b22495cc9fd0f9bc29a5c95b2bc7a3223
Guillermo575 <guille_medina575@hotmail.com>
2026-06-17 00:23:17 -0600

    Update DebugDetection.cs

--- Sudoku/Assets/_Script/DebugDetection.cs
+++ Sudoku/Assets/_Script/DebugDetection.cs
@@ -1,30 +1,23 @@
-
 using UnityEngine;
-
 public class DebugDetection : MonoBehaviour
 {
     void Start()
     {
         #if UNITY_EDITOR
-            // Si estás en el editor, se considera modo debug
             EnableObject(true);
         #else
-            // En construcciones, verificar si es una build de depuración
             if (Debug.isDebugBuild)
             {
                 EnableObject(true);
             }
             else
             {
-                // No es una build de debug, desactiva el objeto
                 EnableObject(false);
             }
         #endif
     }
-
     private void EnableObject(bool isEnabled)
     {
-        // Puedes desactivar el objeto completo o componentes específicos
         gameObject.SetActive(isEnabled);
     }
 }
