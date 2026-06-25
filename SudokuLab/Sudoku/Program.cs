2e020e2920545d85f1af96ae317c6b59011f1abb
Guillermo575 <guille_medina575@hotmail.com>
2026-06-25 00:48:38 -0600

    Update Program.cs
    Pruebas comentadas

--- SudokuLab/Sudoku/Program.cs
+++ SudokuLab/Sudoku/Program.cs
@@ -9,6 +9,11 @@ namespace SudokuML
             Console.OutputEncoding = Encoding.UTF8;
             Console.InputEncoding = Encoding.UTF8;
             Tools.MainMenu.MostrarMenu();
+            //var sudokuRapido = new Sudoku.SudokuGenerator(3, 3, QuickFunction: true);
+            //var sudoku4x4 = new Sudoku.SudokuGenerator(4, 4, QuickFunction: true);
+            //var sudoku5x4 = new Sudoku.SudokuGenerator(5, 4, QuickFunction: true);
+            //var sudoku5x5 = new Sudoku.SudokuGenerator(5, 5, QuickFunction: true);
+            //var sudoku6x6 = new Sudoku.SudokuGenerator(6, 6, QuickFunction: true);
         }
     }
 }
